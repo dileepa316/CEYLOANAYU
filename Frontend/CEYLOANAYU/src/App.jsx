9b6eda0b4c2f6929e6eb4de516afc032089bb0ba
dileepa316 <dileepahashan038@gmail.com>
2025-08-13 21:20:56 +0530

    jjj

--- Frontend/CEYLOANAYU/src/App.jsx
+++ Frontend/CEYLOANAYU/src/App.jsx
@@ -10,15 +10,17 @@ import SurfingWeligama from "./components/Experience1/SurfingWeligama.jsx";
 import GalleFort from "./components/Galle/GalleFort.jsx";
 import MangroveTour from "./components/Koggala/MangroveTour.jsx";
 import WhaleWatching from "./components/mirissa/WhaleWatching.jsx";
-import SignupPage from "./components/Signup/SignupPage.jsx"; // <-- Fixed here
+import SignupPage from "./components/Signup/SignupPage.jsx";
 import ContactPage from "./components/Contactus/ContactPage.jsx";
+import AyurvedaWellness from "./components/AyurvedaWellness/AyurvedaWellness.jsx";
 
 // CSS Imports
 import "./components/Loginpage/Login.css";
-import "./components/Signup/Signup.css"; // <-- matches your pasted CSS filename
+import "./components/Signup/Signup.css";
 import "./components/Koggala/MangroveTour.css";
 import "./components/mirissa/WhaleWatching.css";
 import "./components/Contactus/Contact.css";
+import "./components/AyurvedaWellness/AyurvedaWellness.css";
 import "./App.css";
 
 function App() {
@@ -27,13 +29,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} /> {/* <-- Fixed */}
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/surfing-weligama" element={<SurfingWeligama />} />
         <Route path="/galle-fort" element={<GalleFort />} />
         <Route path="/mangrove-tour" element={<MangroveTour />} />
         <Route path="/whale-watching" element={<WhaleWatching />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/ayurveda-wellness" element={<AyurvedaWellness />} />
       </Routes>
     </Router>
   );
